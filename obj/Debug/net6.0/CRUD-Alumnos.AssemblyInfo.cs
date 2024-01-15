9b2aa2af63b3be19552ca7191e0e2bcb98a4c0a8
vivianmunguia <andy_munguia@hotmail.com>
2024-01-14 19:31:32 -0700

    A constant was created

--- obj/Debug/net6.0/CRUD-Alumnos.AssemblyInfo.cs
+++ obj/Debug/net6.0/CRUD-Alumnos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD-Alumnos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c54d83e9f4124cc411e5ca1621cd14980f6e53ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD-Alumnos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD-Alumnos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
